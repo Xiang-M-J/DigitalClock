c051d64c65318888c1757fbd9bc0d811a21a86fc
XMJ <70873544+xmjseu1@users.noreply.github.com>
2021-02-08 14:20:22 +0800

    添加项目文件。

---
+++ DigitalClock/DigitLine.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DigitalClock
+{
+    
+        public class DigitLine
+        {
+            // 数字7个笔划路径数据数组
+            string[] PathDatas = new string[]
+            {
+        "M3,0 L53,0 L38,15 L18,15 Z",                               //  -
+        "M0,3 L15,18 L15,38 L0,53 Z",                               // |
+        "M41,18 L56,3 L56,53 L41,38 Z",                             //    |
+        "M10,48.5 L18,41 L38,41 L46,48.5 L38,56 L18,56 Z",          //   -
+        "M0,44 L15,60 L15,80 L0,94 Z",                              // |
+        "M41,59 L56,44 L56,94 L41,79 Z",                            //    |
+        "M18,82 L38,82 L53,97 L3,97 Z",                             //  _                    
+        "M0,0 L15,0 L15,15 L0,15Z M0,40 L15,40 L15,55 L0,55 Z"      // :
+            };
+
+            // 路径数组（公共）
+            public Path[] Path0_9 = new Path[10];
+            public Path PathColon = new Path();
+
+            /// <summary>
+            /// 根据digit设置（分配）数字路径数据
+            /// </summary>
+            /// <param name="digit">数字</param>
+            private void SetDigit(int digit)
+            {
+                StringBuilder sbData = new StringBuilder();
+
+                switch (digit)
+                {
+                    case 0:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (i == 3)
+                                continue;
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+                    case 1:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (i == 0 || i == 1 || i == 3 || i == 4 || i == 6)
+                            {
+                                continue;
+                            }
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+
+                    case 2:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (i == 1 || i == 5)
+                            {
+                                continue;
+                            }
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+                    case 3:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (i == 1 || i == 4)
+                            {
+                                continue;
+                            }
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+                    case 4:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (i == 0 || i == 4 || i == 6)
+                            {
+                                continue;
+                            }
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+
+                    case 5:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (i == 2 || i == 4)
+                            {
+                                continue;
+                            }
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+
+                    case 6:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (i == 2)
+                            {
+                                continue;
+                            }
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+
+                    case 7:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (i == 1 || i == 3 || i == 4 || i == 6)
+                            {
+                                continue;
+                            }
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+
+                    case 8:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+                    case 9:
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (i == 4)
+                            {
+                                continue;
+                            }
+                            sbData.Append(PathDatas[i]);
+                        }
+                        break;
+
+                }
+
+                Path0_9[digit].Data = Geometry.Parse(sbData.ToString());
+            }
+            /// <summary>
+            /// 设置冒号
+            /// </summary>
+            /// <param name="color"></param>
+            private void SetColon(Brush color)
+            {
+                PathColon.Fill = color;
+                PathColon.Data = Geometry.Parse(PathDatas[7]);
+            }
+            public DigitLine(Brush brush)
+            {
+                // 初始化路径数组
+                for (int i = 0; i < 10; i++)
+                {
+                Path0_9[i] = new Path
+                {
+                    Fill = brush
+                };
+
+                SetDigit(i);
+                }
+
+                SetColon(brush);
+            }
+            public void SetDigitSkewTransform(double angleX)
+            {
+                SkewTransform stf = new SkewTransform(angleX, 0);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Path0_9[i].RenderTransform = stf;
+                }
+            }
+
+            public void SetColonSkewTransform(double angleX)
+            {
+                SkewTransform stf = new SkewTransform(angleX, 0);
+                PathColon.RenderTransform = stf;
+            }
+        }
+    }
+
---
+++ DigitalClock/MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DigitalClock
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //DispatcherTimer timer = new DispatcherTimer();  // 计时器
+        System.Timers.Timer timer = new System.Timers.Timer(1000);
+        DateTime CurrTime = DateTime.Now;                   // 当前时间
+
+        DigitLine[] digitLine = new DigitLine[6];
+        DigitLine[] digitLineBack = new DigitLine[6];
+
+        DigitLine[] digitColon = new DigitLine[2];
+        DigitLine[] digitColonBack = new DigitLine[2];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 6; i++)
+            {
+                digitLine[i] = new DigitLine(new SolidColorBrush(Color.FromArgb(240, 70 ,130 ,180)));
+                digitLineBack[i] = new DigitLine(new SolidColorBrush(Color.FromArgb(120, 240, 248 ,255)));
+            }
+
+            // 初始化冒号
+            for (int i = 0; i < 2; i++)
+            {
+                digitColon[i] = new DigitLine(Brushes.Black);
+                digitColonBack[i] = new DigitLine(new SolidColorBrush(Color.FromArgb(255, 95, 158, 160)));
+            }
+            // 画数字表背景矩形
+            //DrawDigitRect(new Point(-24, 36),616, 196, Brushes.SteelBlue);
+            //DrawDigitRect(new Point(-16, 44), 600, 180, Brushes.Lavender);
+
+            // 画数字底色
+            DrawDigitTimeBack(new SolidColorBrush(Color.FromArgb(120, 120, 120, 120)));
+
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Enabled = true;
+
+        }
+         private void Image1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Image3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            this.Close();
+        }
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+
+        }
+        private void DrawDigitRect(Point pos, double width, double height, Brush color)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = width;
+            rect.Height = height;
+            rect.Fill = color;
+            Canvas.SetLeft(rect, pos.X);
+            Canvas.SetTop(rect, pos.Y);
+            digitCanvas.Children.Add(rect);
+
+        }
+        /// <summary>
+        /// 显示背景数字（底色）
+        /// </summary>
+        /// <param name="brush"></param>
+        private void DrawDigitTimeBack(Brush brush)
+        {
+            // 第1、2位--小时
+            Canvas.SetLeft(digitLineBack[0].Path0_9[8], 40);
+            Canvas.SetTop(digitLineBack[0].Path0_9[8], 80);
+            digitCanvas.Children.Add(digitLineBack[0].Path0_9[8]);
+
+            Canvas.SetLeft(digitLineBack[1].Path0_9[8], 120);
+            Canvas.SetTop(digitLineBack[1].Path0_9[8], 80);
+            digitCanvas.Children.Add(digitLineBack[1].Path0_9[8]);
+
+            // 冒号
+            Canvas.SetLeft(digitColonBack[0].PathColon, 190);
+            Canvas.SetTop(digitColonBack[0].PathColon, 100);
+            digitCanvas.Children.Add(digitColonBack[0].PathColon);
+
+            // 第二冒号
+            Canvas.SetLeft(digitColonBack[1].PathColon, 370);
+            Canvas.SetTop(digitColonBack[1].PathColon, 100);
+            digitCanvas.Children.Add(digitColonBack[1].PathColon);
+
+            // 3、4位--分钟
+            Canvas.SetLeft(digitLineBack[2].Path0_9[8], 220);
+            Canvas.SetTop(digitLineBack[2].Path0_9[8], 80);
+            digitCanvas.Children.Add(digitLineBack[2].Path0_9[8]);
+
+            Canvas.SetLeft(digitLineBack[3].Path0_9[8], 300);
+            Canvas.SetTop(digitLineBack[3].Path0_9[8], 80);
+            digitCanvas.Children.Add(digitLineBack[3].Path0_9[8]);
+
+            // 5、6位--秒钟
+            Canvas.SetLeft(digitLineBack[4].Path0_9[8], 400);
+            Canvas.SetTop(digitLineBack[4].Path0_9[8], 80);
+            digitCanvas.Children.Add(digitLineBack[4].Path0_9[8]);
+
+            Canvas.SetLeft(digitLineBack[5].Path0_9[8], 480);
+            Canvas.SetTop(digitLineBack[5].Path0_9[8], 80);
+            digitCanvas.Children.Add(digitLineBack[5].Path0_9[8]);
+        }
+        /// <summary>
+        /// 显示数字时间
+        /// </summary>
+        private void DrawDigitTime()
+        {
+            // 删除已在画图区的数字
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (digitCanvas.Children.Contains(digitLine[i].Path0_9[j]))
+                    {
+                        digitCanvas.Children.Remove(digitLine[i].Path0_9[j]);
+                    }
+                }
+            }
+
+            // 小时
+            int hour = CurrTime.Hour;
+            int hour1 = hour / 10;
+            int hour2 = hour % 10;
+            Canvas.SetLeft(digitLine[0].Path0_9[hour1], 40);
+            Canvas.SetTop(digitLine[0].Path0_9[hour1], 80);
+            digitCanvas.Children.Add(digitLine[0].Path0_9[hour1]);
+
+            Canvas.SetLeft(digitLine[1].Path0_9[hour2], 120);
+            Canvas.SetTop(digitLine[1].Path0_9[hour2], 80);
+            digitCanvas.Children.Add(digitLine[1].Path0_9[hour2]);
+
+            // 冒号闪烁
+            if (CurrTime.Second % 2 == 0)
+            {
+                digitColon[0].PathColon.Visibility = Visibility.Visible;
+                digitColon[1].PathColon.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                digitColon[0].PathColon.Visibility = Visibility.Hidden;
+                digitColon[1].PathColon.Visibility = Visibility.Hidden;
+            }
+
+            // 分钟
+            int minute = CurrTime.Minute;
+            int minu1 = minute / 10;
+            int minu2 = minute % 10;
+            Canvas.SetLeft(digitLine[2].Path0_9[minu1], 220);
+            Canvas.SetTop(digitLine[2].Path0_9[minu1], 80);
+            digitCanvas.Children.Add(digitLine[2].Path0_9[minu1]);
+
+            Canvas.SetLeft(digitLine[3].Path0_9[minu2], 300);
+            Canvas.SetTop(digitLine[3].Path0_9[minu2], 80);
+            digitCanvas.Children.Add(digitLine[3].Path0_9[minu2]);
+
+            // 秒钟
+            int second = CurrTime.Second;
+            int sec1 = second / 10;
+            int sec2 = second % 10;
+            Canvas.SetLeft(digitLine[4].Path0_9[sec1], 400);
+            Canvas.SetTop(digitLine[4].Path0_9[sec1], 80);
+            digitCanvas.Children.Add(digitLine[4].Path0_9[sec1]);
+
+            Canvas.SetLeft(digitLine[5].Path0_9[sec2], 480);
+            Canvas.SetTop(digitLine[5].Path0_9[sec2], 80);
+            digitCanvas.Children.Add(digitLine[5].Path0_9[sec2]);
+        }
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            this.Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() =>
+            {
+                CurrTime = DateTime.Now;
+
+                // 更新数字时钟数字
+                DrawDigitTime();
+            }));  
+        }
+    }
+}
